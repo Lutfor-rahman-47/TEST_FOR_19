1e75d5c50216c7344b46c38cbd536f961060771a
somaya21034 <it21034@mbstu.ac.bd>
2025-11-08 10:19:45 +0600

    my wish

--- First.cpp
+++ First.cpp
@@ -3,5 +3,6 @@ using namespace std;
 int main() {
     cout << "Hello, World!" << endl;
     cout << "This is my first C++ program." << endl;
+    cout << "I am learning C++ programming." << endl;
     return 0;
 }
